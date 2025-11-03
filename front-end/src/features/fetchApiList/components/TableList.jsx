f451044719480d0a73275a97f8fcf548fde231fa
gilangherdiansyah80 <gilangherdiansyah404@gmail.com>
2025-11-03 14:53:25 +0700

    new feature : create fetch list

--- front-end/src/features/fetchApiList/components/TableList.jsx
+++ front-end/src/features/fetchApiList/components/TableList.jsx
@@ -1,16 +1,36 @@
-import UseFetch from "../hooks/UseFetch";
+import { useState, useEffect } from "react";
 
 const TableList = () => {
-  const { dataList } = UseFetch;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://pokeapi.co/api/v2/ability");
+      const result = await response.json();
+      setData(result.results);
+    };
+
+    fetchData();
+  }, []);
   return (
-    <main>
-      {dataList.map((item) => {
-        return (
-          <div>
-            <h1>{item.name}</h1>
-          </div>
-        );
-      })}
+    <main className="w-full lg:w-3/5 lg:self-center">
+      <table className="min-w-full text-sm text-gray-700">
+        <thead>
+          <tr className="bg-gray-100 text-gray-600 text-center">
+            <th className="px-6 py-3 font-semibold tracking-wide border-b border-gray-200">
+              Name
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.map((item) => (
+            <tr className="px-6 py-3 border-b border-gray-100 text-4xl">
+              {item.name}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
   );
 };
